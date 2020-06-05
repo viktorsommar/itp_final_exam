18cc13592ba4bacb02a4b96a8b4673942ec93b17
viktor_sommar <vsommarbaillot@gmail.com>
2020-06-05 11:20:50 +0200

    Testing feature of getting a message when clicking the Press Me button. Passed.

--- cypress/integration/userCanGetAMessage.feature.js
+++ cypress/integration/userCanGetAMessage.feature.js
@@ -1,5 +1,12 @@
 describe('user can get a message', () => {
-    it('test', () => {
+
+    before(() => {
         cy.visit('http://localhost:3001')
+    })
+
+    it('by clicking the "Press me" button', () => {
+        cy.get('#message').should('not.be.visible')
+        cy.get('#quote').click()
+        cy.get('#message').should('be.visible')
     })
 })
